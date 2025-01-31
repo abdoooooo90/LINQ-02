95caa1ca794d8946558bed238a1aeef645885bf4
Abdelfatah Saad <120993266+abdoooooo90@users.noreply.github.com>
2025-01-31 10:30:06 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using static Demo.ListGenerator;
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Element Operators
+            #region First - Last
+            //var Result = ProductList.First();
+            //// Get First Element At Sequence
+            //var Result = ProductList.Last();
+            //// Get First Element At Sequence
+            //Console.WriteLine(Result);
+            //-------------------------------
+            //List<Product> products = new List<Product>();
+            //var Result = products.First(); //Unhandled exception. System.InvalidOperationException
+            //Console.WriteLine(Result);
+            //var Result = products.Last(); //Unhandled exception. System.InvalidOperationException
+            //First And Last May Throw Exception => If The Sequence Is Empty
+            //------------------------------------------
+            //var Result = ProductList.First(P => P.UnitsInStock == 0); //Get First Element At Sequence That Match The Condition 
+            //var Result = ProductList.Last(P => P.UnitsInStock == 999);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region FirstOrDefault - LastOrDefault            
+            //List<Product> TestProduct = new List<Product>();
+            //var Result = TestProduct.FirstOrDefault();
+            // Get First Element In Sequence If Sequence Is Empty Will Retrun Null
+            //var Result = TestProduct.LastOrDefault();
+            // Get Last Element In Sequence If Sequence Is Empty Will Retrun Null
+            //Console.WriteLine(Result?.ProductName?? "Not Found");
+            #endregion
+
+            #region ElementAt - ElementAtOrDefault            
+            //var Result = ProductList.ElementAt(77);
+            //Unhandled exception. System.ArgumentOutOfRangeException: Index was out of range.
+            //-------------------------------------------
+            //var Result = ProductList.ElementAtOrDefault(77);
+            //Console.WriteLine(Result?.ProductName?? "Not Found");
+
+            #endregion
+
+            #region Single - SingleOrDefault
+            //var Result = ProductList.Single(P => P.UnitsInStock == 0);
+            // Invalid OperationException : Sequence Contains More Then One Element Math Condition
+            // Single Will Retrun It If  Sequence Contains Only One Element Math Condition 
+            // If The Sequence Contains More Then One Element Math Condition Or The Sequence Is Empty Will Throw Excption
+            //----------------------------------
+            //var Result = ProductList.SingleOrDefault(P => P.UnitsInStock == 0);
+
+            //Console.WriteLine(Result?.ProductName?? "Not Found");
+            #endregion
+
+            #endregion
+
+            #region Aggregate Operators
+            #region Count
+            //var Result = ProductList.Count();
+            //var Result = ProductList.Count(P => P.UnitPrice == 0);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region Min - Max
+            //var Result = ProductList.Min(); //Unhandled exception. System.ArgumentException:
+            //var Result = ProductList.Max();
+            //Console.WriteLine(Result);
+            //--------------------------------
+            //var Result = ProductList.Min(P => P.ProductName.Length);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region Sum - Avg
+            //var Result = ProductList.Sum(P => P.UnitPrice);
+            //var Result = ProductList.Average(P => P.UnitPrice);
+            //Console.WriteLine(Result);
+
+            #endregion
+
+            #region Aggregate
+            //string[] name = { "Ali", "Omar", "Mohamed", "Ahmed" };
+            //var Result = name.Aggregate((str01, str02) => $"{str01} {str02}");
+            //Console.WriteLine(Result);
+            #endregion
+
+            #endregion
+
+            #region Casting Operators
+            //List<Product> Result = ProductList.Where(P => P.UnitsInStock == 0).ToList();
+            //Product[] Result = ProductList.Where(P => P.UnitsInStock == 0).ToArray();
+            //-----------------------------
+            // Dictionary<long, Product> Result = ProductList.Where(P => P.UnitsInStock == 0).ToDictionary(P => P.ProductID);
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            //}
+            //--------------------------
+            //HashSet<Product> Result = ProductList.Where(P => P.UnitsInStock == 0).ToHashSet();
+            //foreach(var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //--------------------------
+            //ArrayList obj = new ArrayList()
+            //{
+            //    "Ali",
+            //    "Moohamed",
+            //    "Abdo",
+            //    1,2,3, 'A'
+            //};
+            //var Result = obj.OfType<string>();
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Generation Operators - Deferred Execution
+            //var Result = Enumerable.Range(0, 100); // 0-99
+            //var Result = Enumerable.Repeat(2, 100);
+            //---------------------
+            //var Result = Enumerable.Empty<Product>().ToArray();
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}                                     
+
+            #endregion
+
+            #region Set Operators
+            var Seq01 = Enumerable.Range(0, 100);
+            var Seq02 = Enumerable.Range(50, 100);
+            #region Union
+            //var Result = Seq01.Union(Seq02);
+            //Console.WriteLine("Seq01");
+            //foreach (var Seq in Seq01)
+            //{
+            //    Console.WriteLine(Seq);
+            //}
+            //Console.WriteLine("Seq02");
+            //foreach (var Seq in Seq02)
+            //{
+            //    Console.WriteLine(Seq);
+            //}
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Count
+            //var Result = Seq01.Concat(Seq02);
+            //Console.WriteLine("Seq01");
+            //foreach (var Seq in Seq01)
+            //{
+            //    Console.WriteLine(Seq);
+            //}
+            //Console.WriteLine("Seq02");
+            //foreach (var Seq in Seq02)
+            //{
+            //    Console.WriteLine(Seq);
+            //}
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Intersect
+            //var Result = Seq01.Intersect(Seq02);
+            //Console.WriteLine("Seq01");
+            //foreach (var Seq in Seq01)
+            //{
+            //    Console.WriteLine(Seq);
+            //}
+            //Console.WriteLine("Seq02");
+            //foreach (var Seq in Seq02)
+            //{
+            //    Console.WriteLine(Seq);
+            //}
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Except
+            //var Result = Seq01.Except(Seq02);
+            //Console.WriteLine("Seq01");
+            //foreach (var Seq in Seq01)
+            //{
+            //    Console.WriteLine(Seq);
+            //}
+            //Console.WriteLine("Seq02");
+            //foreach (var Seq in Seq02)
+            //{
+            //    Console.WriteLine(Seq);
+            //}
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #endregion
+        }
+    }
+}
---
+++ LINQ 02/Program.cs
@@ -0,0 +1,197 @@
+﻿using System.Diagnostics;
+using System.Security.AccessControl;
+using static LINQ_02.ListGenerator;
+namespace LINQ_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Element Operators
+            #region 1. Get first Product out of Stock 
+            //var Result01 = ProductList.First(P => P.UnitsInStock ==0);
+            //Console.WriteLine(Result01);
+            #endregion
+
+            #region 2. Return the first product whose Price > 1000, unless there is no match, in which case null is returned
+            //var Result = ProductList.FirstOrDefault(P => P.UnitPrice > 1000);
+            //Console.WriteLine(Result?.ProductName ?? "Not Found");
+            #endregion
+
+            #region 3. Retrieve the second number greater than 5
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Where(A => A > 5).ElementAt(1);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #endregion
+
+            #region Aggregate Operators
+            #region 1. Uses Count to get the number of odd numbers in the array
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Count(A => A % 2 == 1);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region 2. Return a list of customers and how many orders each has.
+            //var Result = CustomerList.Select(C => new
+            //{
+            //    CustomerName = C.CustomerName,
+            //    OrderCount = C.Orders.Count()
+            //});
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region 3. Return a list of categories and how many products each has
+            //var Result = ProductList.Select(P => new
+            //{
+            //    Category = P.Category,
+            //    ProductCount = P.ProductName.Count()
+            //});
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region 4. Get the total of the numbers in an array.
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Sum();
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region Q5, Q6, Q7, Q8
+            //string path = "C:/Users/abdos/OneDrive/Desktop/.net/C#/LINQ/LINQ 02/LINQ 02/bin/Debug/net8.0/dictionary_english.txt";
+            //string[] content = File.ReadAllLines(path);
+            #region 5. Get the total number of characters of all words in dictionary_english.txt (Read dictionary_english.txt into Array of String First).
+            //var Result = content.Sum(x => x.Length);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region 6. Get the length of the shortest word in dictionary_english.txt (Read dictionary_english.txt into Array of String First).
+            //var Result = content.Min(x => x.Length);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region 7. Get the length of the longest word in dictionary_english.txt (Read dictionary_english.txt into Array of String First)
+            //var Result = content.Max(x => x.Length);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region 8. Get the average length of the words in dictionary_english.txt (Read dictionary_english.txt into Array of String First)
+            //var Result = content.Average(A => A.Length);
+            //Console.WriteLine(Result);
+            #endregion
+
+            #endregion
+
+            #region 9. Get the total units in stock for each product category.
+            //var Total = ProductList.GroupBy(P => P.Category).Select(G => new
+            //{ 
+            //    Category = G.Key, Stock = G.Sum(P => P.UnitsInStock)
+            //});
+            //foreach (var item in Total)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region 10. Get the cheapest price among each category's products
+            //var Result = ProductList.GroupBy(P => P.Category).Select(C => new
+            //{
+            //    Category = C.Key,
+            //    CheapestPrice = C.Min(P => P.UnitPrice)
+            //});
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region 11. Get the products with the cheapest price in each category (Use Let)
+            //var Result = from P in ProductList
+            //             let CheapestPrice = (
+            //             from P2 in ProductList
+            //             where P2.Category == P.Category
+            //             select P2.UnitPrice
+            //             ).Min()
+            //             where P.UnitPrice == CheapestPrice
+            //             select P;
+            //foreach ( var P in Result )
+            //{
+            //    Console.WriteLine( P );
+            //}
+
+            #endregion
+
+            #region 12. Get the most expensive price among each category's products.
+            //var Result = ProductList.GroupBy(P => P.Category).Select(E => new
+            //{
+            //    Category = E.Key,
+            //    ExpensivePrice = E.Max(U => U.UnitPrice)
+            //});
+            //foreach (var result in Result)
+            //{
+            //    Console.WriteLine(result);
+            //}
+            #endregion
+
+            #region 13. Get the products with the most expensive price in each category.
+            //var Result = ProductList.GroupBy(P => P.Category).Select(G => G.OrderByDescending(P => P.UnitPrice).First());
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region 14. Get the average price of each category's products.
+            //var Result = ProductList.GroupBy(C => C.Category).Select(P => new
+            //{
+            //    Cateegory = P.Key,
+            //    AvgPrice = P.Average(P => P.UnitPrice)
+            //});
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #endregion
+
+            #region Set Operators
+            #region 1. Find the unique Category names from Product List
+            //var Result = ProductList.Select(P => P.Category).Distinct();
+            //foreach ( var item in Result ) 
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region 2. Produce a Sequence containing the unique first letter from both product and customer names.
+            //var Result = ProductList.Select(P => P.ProductName[0]).Union(CustomerList.Select(C => C.CustomerName[0])).Distinct();
+            //foreach(var item  in Result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region 3. Create one sequence that contains the common first letter from both product and customer names.
+            //var Result = ProductList.Select(P => P.ProductName[0]).Intersect(CustomerList.Select(C => C.CustomerName[0]));
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region 4. Create one sequence that contains the first letters of product names that are not also first letters of customer names.
+            //var Result = ProductList.Select(P => P.ProductName[0]).Except(CustomerList.Select(C => C.CustomerName[0]));
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region 5. Create one sequence that contains the last Three Characters in each name of all customers and products, including any duplicates
+            //var Result = ProductList.Select(P => P.ProductName.Length >= 3 ? P.ProductName.Substring(P.ProductName.Length - 3) : P.ProductName).Concat(CustomerList.
+            //    Select(C => C.CustomerName.Length >= 3? C.CustomerName.Substring(C.CustomerName.Length - 3) : C.CustomerName));
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #endregion
+        }
+    }
+}
